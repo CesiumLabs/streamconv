31498967c5686a51ef4ce47775f87c863eea25ca
JingAn Chen <chen.jingan@icloud.com>
2023-08-27 12:10:02 +0800

    Using the latest Deno syntax for Node

--- mod.ts
+++ mod.ts
@@ -2,8 +2,8 @@ import { convert as WebToReadable } from "./src/Conv.ts";
 import { toWebStream as ReadableToWeb } from "./src/toWeb.ts";
 
 export const Converter = {
-    WebToReadable,
-    ReadableToWeb
-}
+	WebToReadable,
+	ReadableToWeb,
+};
 
 export default Converter;
